eb4592d896161407bdc461f27767fb2aa8cc9271
TheTomWard <tom.ward@thekeysupport.com>
2019-03-06 15:10:07 +0000

    Implemeting Machine Learning

--- composeexample/urls.py
+++ composeexample/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
     url(r'^blog/', include('blog.urls')),
+    url(r'^ml/', include('ml.urls')),
     url(r'^api/', include('snippets.urls')),
 ]
